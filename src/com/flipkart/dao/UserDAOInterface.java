ed62a204a2877caa147416dddd9272567cf09ae6
Abhav Mehrotra <abhav.mehrotra@BLRETV-YKM969FL.local>
2023-09-18 12:59:56 +0530

    added comments

--- src/com/flipkart/dao/UserDAOInterface.java
+++ src/com/flipkart/dao/UserDAOInterface.java
@@ -9,9 +9,35 @@ import com.flipkart.bean.User;
  * @author kshitij.gupta1
  */
 public interface UserDAOInterface {
+
+	/**
+	 * Dao method to insert user into user DB
+	 * @param user
+	 */
 	public void insertUserDB(User user);
+
+
+	/**
+	 * Dao method to update user details in UserDB
+	 * @param user
+	 * @param password
+	 */
 	public void updateUserDB(User user,String password);
+
+
+	/**
+	 * Dao method to check whether a user exists for given details without password
+	 * @param user
+	 * @return whether a user exists for given details
+	 */
 //	public User getUserDB(User user);
 	public Boolean queryUserDB(User user);
+
+
+	/**
+	 * Dao method to check whether a user exists for given details with password
+	 * 	 * @param user
+	 * 	 * @return whether a user exists for given details
+	 */
 	public Boolean queryDetailsUserDB(User user);
 }
